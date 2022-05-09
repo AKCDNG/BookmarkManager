a9e569406e71fcba48a9be313fc33528ce584cd0
Anish <anishkakaiya@outlook.com>
2022-05-09 17:54:51 +0100

    update bookmark to use database

--- lib/bookmarklist.rb
+++ lib/bookmarklist.rb
@@ -1,11 +1,9 @@
 class BookmarkList
 
   def self.all
-    [
-      "http://www.makersacademy.com",
-      "http://www.destroyallsoftware.com",
-      "http://www.google.com"
-     ]
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec("SELECT * FROM bookmarks;")
+    result.map { |bookmark| bookmark['url'] }
   end
 
 end
--- spec/unit/bookmarklist_spec.rb
+++ spec/unit/bookmarklist_spec.rb
@@ -4,9 +4,9 @@ describe BookmarkList do
   describe '.all' do
     it 'it returns all the bookmarks' do
       bookmark_list = BookmarkList.all
-      expect(bookmark_list).to include("http://www.makersacademy.com")
-      expect(bookmark_list).to include("http://www.destroyallsoftware.com")
-      expect(bookmark_list).to include("http://www.google.com")
+      expect(bookmark_list).to include("http://www.makersacademy.com/")
+      expect(bookmark_list).to include("http://www.destroyallsoftware.com/")
+      expect(bookmark_list).to include("http://www.google.com/")
     end
   end
 end
