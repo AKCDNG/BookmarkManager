2d634262adb4388d0c3a2eab7aa4da05efcbc3e1
Anish <anishkakaiya@outlook.com>
2022-05-10 16:46:34 +0100

    sets up test database

--- spec/features/bookmarks_content_spec.rb
+++
@@ -1,8 +0,0 @@
-feature "see bookmarks" do
-  scenario "homepage has see bookmarks" do
-    visit("/bookmarks")
-    expect(page).to have_content "http://www.makersacademy.com"
-    expect(page).to have_content "http://www.destroyallsoftware.com"
-    expect(page).to have_content "http://www.google.com"
-  end
-end
--- spec/unit/bookmarklist_spec.rb
+++ spec/unit/bookmarklist_spec.rb
@@ -3,6 +3,14 @@ require 'bookmarklist'
 describe BookmarkList do
   describe '.all' do
     it 'it returns all the bookmarks' do
+
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+
+      # Add the test data
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com/');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com/');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com/');")
+  
       bookmark_list = BookmarkList.all
       expect(bookmark_list).to include("http://www.makersacademy.com/")
       expect(bookmark_list).to include("http://www.destroyallsoftware.com/")
